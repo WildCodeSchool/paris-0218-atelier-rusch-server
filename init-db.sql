75bd27916766ae45163fa41b9cec3aa39196fd20
Christopher Fremond <christopher.fremond@gmail.com>
2018-05-31 10:29:24 +0200

    1st push

---
+++ init-db.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS rusch;
+CREATE DATABASE rusch;
+USE rusch;
+CREATE TABLE homepage (
+	id INT NOT NULL AUTO_INCREMENT,
+	title varchar(64),
+	description varchar(1024),
+	PRIMARY KEY (id)
+);
+
+INSERT INTO homepage (title, description)
+	VALUES ("notre titre", "notre description");
+
+SELECT * FROM homepage;
