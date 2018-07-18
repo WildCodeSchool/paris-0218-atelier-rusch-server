2bc972fe648195b245a1e746d948cad75e394765
root <root@vps541480.ovh.net>
2018-07-18 18:15:51 +0200

    default port 5000

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const db = require('./db')
-const port = process.env.PORT || 3456
+const port = process.env.PORT || 5000
 
 const app = express()
 
